e8a1255463d2a4b486dea1c8d51bb6da06553d51
Senya Senya <senyasenya010896@gmail.com>
2024-06-12 15:04:46 +0300

    start configuration

---
+++ lua/core/mappings.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+
+-- NeoTree
+vim.keymap.set('n', '<leader>e', ':Neotree float reveal<CR>')
+vim.keymap.set('n', '<leader>E', ':Neotree left reveal<CR>')
+vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
